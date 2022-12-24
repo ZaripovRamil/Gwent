2ff5fda913fe7d03e2115debf702bf0db08edc9b
H0TC00LER <112895111+H0TC00LER@users.noreply.github.com>
2022-12-24 22:56:31 +0300

    refactored some views

---
+++ Gwent/GwentClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ReactiveUI;
+using MessageBox.Avalonia;
+using Protocol.Serializator;
+using Protocol;
+using GwentClient.Models.Dtos;
+
+namespace GwentClient.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public MainWindowViewModel MainWindow { get; set; }
+
+        private string login;
+        public string Login
+        {
+            get => login;
+            set=> this.RaiseAndSetIfChanged(ref login, value);
+        }
+
+        public void SendLogin()
+        {
+            if (Login == null || Login.Length < 3 || Login.Length > 8)
+            {
+                var dialog = MessageBoxManager.GetMessageBoxStandardWindow("Ошибка!", "Логин должен быть от 3 до 8 символов.");
+                dialog.Show();
+                return;
+            }
+
+            MainWindow.Client.QueuePacketSend(
+                XPacketConverter
+                .Serialize(XPacketType.GameRequest, new GameStartRequest(Login))
+                .ToPacket());
+        }
+
+        public LoginViewModel(MainWindowViewModel mainWindow)
+        {
+            MainWindow = mainWindow;
+        }
+    }
+}
--- Gwent/GwentClient/ViewModels/MainWindowViewModel.cs
+++ Gwent/GwentClient/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,52 @@
-﻿using System.Collections.Generic;
+﻿using Protocol;
+using System;
+using System.Collections.Generic;
+using TCPClient;
 
 namespace GwentClient.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public GameFieldViewModel GameField { get; }
+        public LoginViewModel Login { get; }
+        public XClient Client;
         public MainWindowViewModel()
         {
-            GameField = new GameFieldViewModel(new List<int>() { 1, 2, 3, 4, 5, 6 }, "Пупсич", "Папич");
+            //var client = new XClient();
+            //Client = client;
+
+            //client.OnPacketRecieve += OnPacketRecieve;
+            //client.Connect("127.0.0.1", 4910);
+
+            //Login = new LoginViewModel(this);
+
+            GameField = new GameFieldViewModel(new List<int> { 1, 2, 3, 4, 5, 6, 1, 2 }, "Кексич", "PisiHunt");
+        }
+
+        private static void OnPacketRecieve(byte[] packet)
+        {
+            var parsed = XPacket.Parse(packet);
+
+            if (parsed != null)
+            {
+                ProcessIncomingPacket(parsed);
+            }
+        }
+
+        private static void ProcessIncomingPacket(XPacket packet)
+        {
+            var type = XPacketTypeManager.GetTypeFromPacket(packet);
+
+            switch (type)
+            {
+                case XPacketType.Handshake:
+                    
+                    break;
+                case XPacketType.Unknown:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
